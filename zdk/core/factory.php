578d072d57384b8679d0515a3073c5bfdde96256
SandoDev <camilo.sandoval@zinobe.com>
2020-07-06 17:38:32 -0500

    updated factory

--- zdk/core/factory.php
+++ zdk/core/factory.php
@@ -11,7 +11,9 @@ class Factory
     public function getInstance()
     {
         $pathService = '/../zinobe/' . $this->service . '/client.php';
-        if (file_exists(substr_replace($pathService, 'zdk', 0, 3))) {
+        $path = getcwd();
+        $filePath1 = $path . '/vendor/zinobe/zdk/' . 'zdk/zinobe/' . $this->service . '/client.php';
+        if (file_exists($filePath1) or file_exists(substr_replace($pathService, 'zdk', 0, 3))) {
             require(__DIR__ . $pathService);
             $clientService = new Client();
             return $clientService;
